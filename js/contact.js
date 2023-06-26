30ca4db7aad6a7e7764e9b258b4585583ee9f324
Nayan <nnhore31@gmail.com>
2023-06-26 22:02:11 +0530

    Home page modified

--- js/contact.js
+++ js/contact.js
@@ -8,7 +8,10 @@ function sendMail() {
 
   const s_id = "service_b1d1aw8";
   const t_id = "template_rfvuell";
-
+  if ((document.getElementById("first").value = " ")|| (document.getElementById("number").value=" ") ||  (document.getElementById("mail").value=" ")||(document.getElementById("message").value=" ")) {
+    alert("Please Fill The Spaces In Form First");
+    }
+else{
   emailjs.send(s_id, t_id, form).then((res) => {
     first: document.getElementById("first").value = "";
     number: document.getElementById("number").value = "";
@@ -17,10 +20,12 @@ function sendMail() {
     alert("Submit Successfully ! Our Team will Contact you Shortly !");
   });
 }
+}
 
 let subscribe = document.getElementById("subs");
-subscribe.addEventListener("click", () => {
-  if (subscribe) {
-    alert("Thank You, We'll give you updates on time");
-  }
-});
+
+ subscribe.addEventListener("click", () => {
+    if (subscribe) {
+      alert("Thank You, We'll give you updates on time");
+    }
+  });
