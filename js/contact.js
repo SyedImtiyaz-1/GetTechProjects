2dcc9137b579fbe905c5e61cdf229cba6281ebfe
Nayan <nnhore31@gmail.com>
2023-06-27 17:18:32 +0530

    improved Login Js

--- js/contact.js
+++ js/contact.js
@@ -8,30 +8,30 @@ function sendMail() {
 
   const s_id = "service_b1d1aw8";
   const t_id = "template_rfvuell";
-  if((document.getElementById("first").value=" ")|| (document.getElementById("number").value=" ")||(document.getElementById("mail").value=" ")||(document.getElementById("mail").value=" ")||(document.getElementById("message").value="")){
+  if ((document.getElementById("first").value = " ") || (document.getElementById("number").value = " ") || (document.getElementById("mail").value = " ") || (document.getElementById("mail").value = " ") || (document.getElementById("message").value = "")) {
     alert("Fill up the Form First Then Try To Submit");
-    }
-    else{
-      emailjs.send(s_id, t_id, form).then((res) => {
-        first: document.getElementById("first").value = "";
-        number: document.getElementById("number").value = "";
-        mail: document.getElementById("mail").value = "";
-        message: document.getElementById("message").value = "";
-        alert("Submit Successfully ! Our Team will Contact you Shortly !");
-      });
-    }
+  }
+  else {
+    emailjs.send(s_id, t_id, form).then((res) => {
+      first: document.getElementById("first").value = "";
+      number: document.getElementById("number").value = "";
+      mail: document.getElementById("mail").value = "";
+      message: document.getElementById("message").value = "";
+      alert("Submit Successfully ! Our Team will Contact you Shortly !");
+    });
+  }
 
-  
+
 }
 
 
 let subscribe = document.getElementById("subs");
 let tval = document.getElementById("tval")
 subscribe.addEventListener("click", () => {
-  if (subscribe= " ") {
-    alert("Please Enter Valid Email !");
+  if (subscribe = " ") {
+    tval.value="Enter Valid Email !";
   }
-  else{
+  else {
     alert("Thank You, We'll give you updates on time");
     tval.value = " ";
   }
--- login/login.js
+++ login/login.js
@@ -30,7 +30,6 @@ loginBtn.addEventListener("click", () => {
   form.submit();
 });
 
-// Logout
 let params = {};
 let regex = /([^&=]+)=([^&]*)/g,
   temp;
@@ -62,7 +61,9 @@ fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
     document.getElementById("name").innerHTML += info.name;
     document.getElementById("name").classList.add("nameCSS");
     document.getElementById("image").setAttribute("src", info.picture);
+    document.getElementById('login').textContent='Logout';
 });
+
 function logout() {
   fetch("https://oauth2.googleapis.com/revoke?token=" + info["access_token"], {
     method: "POST",
