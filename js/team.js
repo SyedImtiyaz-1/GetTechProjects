404dac61839b48861134c9fa224ca68d9ef58bf3
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-07-01 12:28:49 +0530

    fixed some issues

--- js/team.js
+++ js/team.js
@@ -1,6 +1,16 @@
 let founder = document.getElementById("founder");
 let co_founder = document.getElementById('co-founder');
 
+
+$(document).ready(function(){
+
+  $('.fa-bars').click(function(){
+     $(this).toggleClass('fa-times');
+     $('.navbar').toggleClass('nav-toggle');
+ });
+});
+
+
 founder.addEventListener('click', ()=>{
   window.open('https://linkedin.com/in/imtiyaz-sde/')
 })
