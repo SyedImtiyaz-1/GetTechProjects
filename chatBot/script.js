ea5831009c30b27d81c2ab23a2c2ce96313079df
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-08-22 00:05:30 +0530

    ChatBot

--- chatBot/script.js
+++ chatBot/script.js
@@ -44,7 +44,7 @@ const generateResponse = (incomingChatLi) => {
     .catch((error) => {
       console.log('API error:', error);
       messageElement.classList.add('error');
-      messageElement.textContent = 'Oops! Please try again!';
+      messageElement.textContent = 'Under Maintenance, Please try again after sometime !';
     })
     .finally(() => chatBox.scrollTo(0, chatBox.scrollHeight));
 };
