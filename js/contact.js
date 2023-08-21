6d389b5e3601fa872cc2ecfac89aacf9f060dd94
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-08-21 19:42:03 +0530

    done

--- js/contact.js
+++ js/contact.js
@@ -8,31 +8,31 @@ function sendMail() {
 
   const s_id = "service_b1d1aw8";
   const t_id = "template_rfvuell";
-  if ((document.getElementById("first").value = " ") || (document.getElementById("number").value = " ") || (document.getElementById("mail").value = " ") || (document.getElementById("mail").value = " ") || (document.getElementById("message").value = "")) {
+  if (
+    form.first === "" ||
+    form.number === "" ||
+    form.mail === "" ||
+    form.message === ""
+  ) {
     alert("Fill up the Form First Then Try To Submit");
-  }
-  else {
+  } else {
     emailjs.send(s_id, t_id, form).then((res) => {
-      first: document.getElementById("first").value = "";
-      number: document.getElementById("number").value = "";
-      mail: document.getElementById("mail").value = "";
-      message: document.getElementById("message").value = "";
-      alert("Submit Successfully ! Our Team will Contact you Shortly !");
+      document.getElementById("first").value = "";
+      document.getElementById("number").value = "";
+      document.getElementById("mail").value = "";
+      document.getElementById("message").value = "";
+      alert("Submit Successfully! Our Team will Contact you Shortly!");
     });
   }
-
-
 }
 
-
 let subscribe = document.getElementById("subs");
-let tval = document.getElementById("tval")
+let tval = document.getElementById("tval");
 subscribe.addEventListener("click", () => {
-  if (subscribe = " ") {
-    tval.value="Enter Valid Email !";
-  }
-  else {
+  if (tval.value === "") {
+    tval.value = "Enter Valid Email!";
+  } else {
     alert("Thank You, We'll give you updates on time");
-    tval.value = " ";
+    tval.value = "";
   }
 });
