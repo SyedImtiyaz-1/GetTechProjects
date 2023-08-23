e466859d0a023630c62638954f99fdd7466fa407
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-08-23 11:34:57 +0530

    Backend for project

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,21 @@ const purchasedProjects = [
   // ... other projects ...
 ];
 
-// Serve the default home page from the "home" directory
-app.use(express.static('home', { index: 'index.html' }));
-
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
 // Define API routes
+app.get('/api/purchased-projects', (req, res) => {
+  res.json(purchasedProjects);
+});
+
+// Serve the default home page from the "home" directory
+app.use(express.static('home'));
+
+// Handle root URL
 app.get('/', (req, res) => {
-  console.log('Accessed root URL');
-  res.send('Hello from the backend server!');
+  // Since the default home page is served using the static middleware, it will be served automatically
+  // No need to explicitly send a response here
 });
 
 app.listen(PORT, () => {
