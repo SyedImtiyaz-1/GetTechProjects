fdc829e7a751edecf5f5631ec081bc3e8b9935c4
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-08-24 18:38:06 +0530

    All pages are ready

--- backend/public/projects.js
+++ backend/public/projects.js
@@ -1,51 +1,66 @@
-const coursesContainer = document.querySelector(".courses");
-
 document.addEventListener("DOMContentLoaded", async () => {
+  const searchInput = document.getElementById("searchProjects");
+  const coursesContainer = document.querySelector(".courses");
+  const projectData = []; // Store project data for reference
+
   // Fetch new projects data from the JSON file
   const response = await fetch("./projects.json");
   const newProjectsData = await response.json();
 
   newProjectsData.forEach((project) => {
-    addProjectToUI(project);
+    const projectElement = addProjectToUI(project);
+    projectData.push({ element: projectElement, data: project });
   });
-});
 
-function addProjectToUI(project) {
-  const projectElement = document.createElement("div");
-  projectElement.classList.add("course");
-  projectElement.setAttribute("data-course-id", project.courseId); // Use project.courseId instead of project.id
+  searchInput.addEventListener("input", function () {
+    const searchTerm = searchInput.value.trim().toLowerCase();
 
-  projectElement.innerHTML = `
+    projectData.forEach(({ element, data }) => {
+      const projectName = data.name.toLowerCase();
+      
+      const matchesSearch = projectName.includes(searchTerm);
+
+      element.style.display = matchesSearch ? "block" : "none";
+    });
+  });
+
+  function addProjectToUI(project) {
+    const projectElement = document.createElement("div");
+    projectElement.classList.add("course");
+    projectElement.setAttribute("data-course-id", project.courseId);
+
+    projectElement.innerHTML = `
       <div class="course-banner">
-        <!-- Project image -->
         <img src="${project.image}" alt="${project.name}">
       </div>
       <div class="course-detail">
         <h3 class="course-title">${project.name}</h3>
         <p>${project.techstack}</p>
         <div class="course-info">
-          <button id="buyButton" class="buy-button course" data-paid="false">Buy Course</button>
+          <button class="buy-button course" data-paid="false">Buy Course</button>
           <a class="download-link btn btn-7 btn-7c btn-icon-only zmdi-arrow-right" href="${project.downloadLink}" download style="display: none;">Download Project</a>
         </div>
       </div>
     `;
 
-  coursesContainer.appendChild(projectElement);
+    coursesContainer.appendChild(projectElement);
 
-  const buyButton = projectElement.querySelector(".buy-button");
-  const downloadLink = projectElement.querySelector(".download-link");
+    const buyButton = projectElement.querySelector(".buy-button");
+    const downloadLink = projectElement.querySelector(".download-link");
 
-  buyButton.addEventListener("click", async function (event) {
-    const courseId = project.courseId;
-    const paid = this.getAttribute("data-paid");
-  
-    if (paid === "false") {
-      await handlePayment(courseId, downloadLink, buyButton);
-    } else {
-      alert(`You've already purchased the ${courseId} project.`);
-    }
-  });  
-}
+    buyButton.addEventListener("click", async function (event) {
+      const courseId = project.courseId;
+      const paid = this.getAttribute("data-paid");
+
+      if (paid === "false") {
+        await handlePayment(courseId, downloadLink, buyButton);
+      } else {
+        alert(`You've already purchased the ${courseId} project.`);
+      }
+    });
+    return projectElement;
+  }
+});
 
 async function handlePayment(courseId, downloadLink, buyButton) {
   // Simulate payment process using Razorpay
