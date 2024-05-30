b64d20fb7e14e761227b8f988f9bc223891c5637
kashnx <kashnii250@gmail.com>
2024-05-30 17:05:31 +0530

    made scrollbar and cross button separate

--- main.js
+++ main.js
@@ -24,20 +24,7 @@ $(document).ready(function(){
 
    (function ($) {
     "use strict";
-    
-    // $(".clients-carousel").owlCarousel({
-    //     autoplay: true,
-    //     dots: true,
-    //     loop: true,
-    //     responsive: { 0: {items: 2}, 768: {items: 4}, 900: {items: 6} }
-    // });
 
-    // $(".testimonials-carousel").owlCarousel({
-    //     autoplay: true,
-    //     dots: true,
-    //     loop: true,
-    //     responsive: { 0: {items: 1}, 576: {items: 2}, 768: {items: 3}, 992: {items: 4} }
-    // });
     
 })(jQuery);
 })
