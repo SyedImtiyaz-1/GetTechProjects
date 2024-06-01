cdf8c32f5bf48fd6d210ac5b20807e434cb7a534
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2024-06-02 02:01:29 +0530

    Revert "Fixed 🐞: Persist Dark Mode/Light Mode Preference Across Page Refreshes"

--- js/theme.js
+++ js/theme.js
@@ -1,24 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const content = document.getElementsByTagName('body')[0];
-    const toggle = document.getElementById('toggle');
 
-    // Check for saved dark mode preference in localStorage
-    const darkModeEnabled = localStorage.getItem('darkMode') === 'true';
+const content = document.getElementsByTagName('body')[0];
+const darkMode = document.getElementById('toggle')
 
-    if (darkModeEnabled) {
-        toggle.classList.add('active');
-        content.classList.add('night');
-    } else {
-        toggle.classList.remove('active');
-        content.classList.remove('night');
-    }
-
-    // Toggle dark mode on button click
-    toggle.addEventListener('click', function() {
-        const darkModeEnabled = content.classList.toggle('night');
-        toggle.classList.toggle('active');
-
-        // Save dark mode state in localStorage
-        localStorage.setItem('darkMode', darkModeEnabled);
-    });
-});
+toggle.addEventListener('click', function(){
+    toggle.classList.toggle('active');
+    content.classList.toggle("night")
+})
